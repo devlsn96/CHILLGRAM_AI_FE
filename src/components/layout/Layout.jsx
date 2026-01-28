3587727c0b7fce1d634532529d6d18dd7e852ed0
taemin208 <tae6633@naver.com>
2026-01-28 13:22:22 +0900

    refactor(layout): 회원가입 페이지 헤더/푸터 숨김

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -7,18 +7,20 @@ export function Layout({ children, isLoggedIn, setIsLoggedIn }) {
   const { pathname } = useLocation();
 
   const isAdCreatePage = pathname === "/dashboard/createAD";
+  const isAuthOnlyPage = pathname.startsWith("/signup");
 
   return (
     // 선우님과 충돌 해결을 위해 min-h-dvh로 수정 했습니다
     <div className="min-h-dvh flex flex-col bg-white w-full">
-      {isAdCreatePage ? (
-        <CreateADHeader />
-      ) : (
-        <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
-      )}
+      {!isAuthOnlyPage &&
+        (isAdCreatePage ? (
+          <CreateADHeader />
+        ) : (
+          <Header isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />
+        ))}
 
       <main className="flex-1">{children}</main>
-      <Footer />
+      {!isAuthOnlyPage && <Footer />}
     </div>
   );
 }
